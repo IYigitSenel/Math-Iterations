459664ab4175115e000d4c826c881186ec94323c
İlhan Yiğit Şenel <ilhanyigit98@gmail.com>
2026-07-14 17:31:24 +0300

    NewtonRaphson

--- NewtonRaphson.cpp
+++ NewtonRaphson.cpp
@@ -4,11 +4,6 @@
 
 using namespace std;
 
-/*
- * This program demonstrates the Newton-Raphson Method.
- * It finds the root of the function f(x) = x^2 - 2 (which is the square root of 2).
- */
-
 double f(double x) {
     return (x * x) - 2.0;
 }
@@ -18,10 +13,10 @@ double derivative_f(double x) {
 }
 
 int main() {
-    double x = 1.0; 
-    double epsilon = 0.000001; 
+    double x = 1.0;
+    double epsilon = 0.000001;
     int step = 1;
-    int max_iterations = 50; 
+    int max_iterations = 50;
 
     cout << fixed << setprecision(6);
     cout << "--- Newton-Raphson Iteration Starting ---" << endl;
@@ -31,7 +26,7 @@ int main() {
         double dfx = derivative_f(x);
 
         if (dfx == 0.0) {
-            cout << "Turev sifir oldu, matematiksel hata! Islem durduruldu." << endl;
+            cout << "Derivative is zero, mathematical error! Process stopped." << endl;
             break;
         }
 
@@ -40,11 +35,13 @@ int main() {
         cout << "Step " << step << ": x = " << x << " -> New x = " << next_x << endl;
 
         if (abs(next_x - x) < epsilon) {
-            cout << "\nKok basariyla bulundu! Deger: " << next_x << endl;
+            cout << "\nRoot successfully found! Value: " << next_x << endl;
             break;
         }
+
         x = next_x;
         step++;
     }
+
     return 0;
 }
